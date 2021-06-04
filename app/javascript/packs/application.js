a741598ef4ff943a2fbc440fb1614f76a5107634
tomichihi <tsshc820@gmail.com>
2021-06-04 15:41:54 +0900

    修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,8 @@ require("@rails/ujs").start()
 //require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("../card")    //追加
+require("../card")
+require()    //追加
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
