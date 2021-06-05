e3481f4a7023ea9c6a5a86e928acbbf7defac563
tomichihi <tsshc820@gmail.com>
2021-06-05 16:34:16 +0900

    Revert "デプロイのテスト"
    This reverts commit e9df3cc5ff2cec3561b0ee5d34f56c64feb319d8.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../card")
-//require()    //追加
+require()    //追加
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
