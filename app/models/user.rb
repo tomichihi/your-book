7bff9a225c6c4025b3c0bd387f8384d0b8691b66
tomichihi <tsshc820@gmail.com>
2021-06-08 18:39:41 +0900

    READMEの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :intros
-  has_many  :likes
+  has_many :likes
   has_one  :profile
   has_one  :payment
   has_many :user_rooms
   has_many :rooms, through: :user_rooms  
   has_many :messages
-
-  validates :name, presence: true
+  with_options presence: true do
+    validates :name, length: { maximum: 8 }
+    validates :password
+    validates :password_confirmation
+  end
+    validates :password, :password_confirmation, length: { minimum: 6 }, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/ }
 
   def liked_by?(intro_id)
     likes.where(intro_id: intro_id).exists?
